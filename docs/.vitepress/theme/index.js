1f01c95e69f9f6e456110214f9b3ddaec4f08773
chunge <chunge1945@outlook.com>
2024-10-21 11:55:51 +0800

    build: :package: 升级 vitepress的版本

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,8 +1,6 @@
 // https://vitepress.dev/guide/custom-theme
 import { VPBTheme } from '@chunge16/vitepress-blogs-theme';
 
-import './style.css';
-
 export default {
   extends: VPBTheme,
   enhanceApp({ app, router, siteData }) {
